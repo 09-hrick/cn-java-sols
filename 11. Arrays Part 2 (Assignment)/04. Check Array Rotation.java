3a99d6b210c79c26c633771c9068233105dd5f47
Sanyam Mahajan <78165562+09hrick@users.noreply.github.com>
2021-02-12 00:20:18 +0530

    Delete 11. Arrays Part 2 (Assignment) directory

--- 11. Arrays Part 2 (Assignment)/04. Check Array Rotation.java
+++
@@ -1,9 +0,0 @@
-public class Solution { 
-    public static int arrayRotateCheck(int[] arr){
-        for(int i = 0; i < arr.length - 1; i++){ 
-            if(arr[i] > arr[i+1])
-                return (i+1); 
-        }
-        return 0; 
-    }
-}
