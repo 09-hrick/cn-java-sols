268da9b4ebec36a578a6125190375a84fda41144
Sanyam Mahajan <67626072+09-hrick@users.noreply.github.com>
2021-02-19 20:53:07 +0530

    Update 3. Arrange Numbers In Array.java

--- 09. Arrays Part 1/3. Arrange Numbers In Array.java
+++ 09. Arrays Part 1/3. Arrange Numbers In Array.java
@@ -1,66 +1,39 @@
-from sys import stdin
-
-def arrange(arr, n) :
-    #Your code goes here
-    if n!=0:
-        if n%2==0:
-            i=1
-            j=0
-            while j<=n/2:
-                arr[j]=i
-                i+=2
-                j+=1
-            k=2
-            q=-1
-            while q >= -n/2:
-                arr[q]=k
-                k+=2
-                q-=1       
-
-        else:
-            i=1
-            j=0
-            while j<=(n-1)/2:
-                arr[j]=i
-                i+=2
-                j+=1
-            k=2
-            q=-1
-            while q >= (1-n)/2:
-                arr[q]=k
-                k+=2
-                q-=1   
-
-    return arr
+import java.util.Scanner;
+public class Solution {
+    
+    public static void arrange(int[] arr, int n) {
+    	                 /* MADE BY RUDRAKSH*/
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#to print the array/list
-def printList(arr, n) :
-    for i in range(n) :
-        print(arr[i], end = ' ')
-    print()
-
-
-
-#main
-t = int(stdin.readline().strip())
-
-while t > 0 :
-    n = int(stdin.readline().strip())
-    arr = n * [0]
-    arrange(arr, n)
-    printList(arr, n)
-    t -= 1
+        int i;
+		int k=0;
+		
+		for(i=1;i<=n;i+=2) {
+			arr[k]=i;
+			k+=1;
+		}
+		i=i-2;
+        if (i==n){
+            i-=1;
+        }
+        else{
+            i+=1;
+        }
+		
+		for(int m=i;m>=2;m-=2) {
+			arr[k]=m;
+			k+=1;
+		}
+    }
+    
+    public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		int cases=s.nextInt();
+        
+		for(int i=1;i<=cases;i++) {
+			int len=s.nextInt();
+            int[] arr=new int[len];
+			arrange(arr,len);
+		}
+	}
+}
