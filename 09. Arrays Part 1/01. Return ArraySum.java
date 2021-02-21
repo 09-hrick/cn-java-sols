b5f81ab2cd9191a41b56e4a43973cee9129c5b16
Sanyam Mahajan <67626072+09-hrick@users.noreply.github.com>
2021-02-21 12:59:43 +0530

    Update and rename 1. return ArraySum.java to 01. Return ArraySum.java

---
+++ 09. Arrays Part 1/01. Return ArraySum.java
@@ -0,0 +1,13 @@
+
+public class Solution {
+	
+	public static int sum(int[] arr) {
+		 int sum=0;
+        for(int i =0;i<arr.length;i++){
+            
+            sum = sum + arr[i];
+        }
+      return sum;
+		
+	}
+}
