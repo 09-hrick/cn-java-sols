7eec7fd127de4ee6d98bb7709376319aa0f7527f
Sanyam Mahajan <67626072+09-hrick@users.noreply.github.com>
2021-02-23 10:55:13 +0530

    Add files via upload

--- 09. Arrays Part 1/10. Sort01.java
+++ 09. Arrays Part 1/10. Sort01.java
@@ -1,10 +1,10 @@
-public class Solution { public static void sortZeroesAndOne(int[] arr) 
-{
-    
-    int nextZero = 0; for(int i = 0; i < arr.length; i++) { if(arr[i] == 0) 
-    {
+public class Solution { 
+    public static void sortZeroesAndOne(int[] arr){
+        int nextZero = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == 0){
         int temp = arr[nextZero]; arr[nextZero] = arr[i]; arr[i] = temp; nextZero += 1;
+            }
+        }
     } 
-                                                          } 
-} 
-                      }
+}
