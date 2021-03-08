8dccd07f437edf6dc154ecfb641ed89a4633f0b1
Sanyam Mahajan <67626072+09-hrick@users.noreply.github.com>
2021-03-08 13:34:12 +0530

    Update 1. Nth Fibonacci Number.java

--- 06. Operators & For Loop/1. Nth Fibonacci Number.java
+++ 06. Operators & For Loop/1. Nth Fibonacci Number.java
@@ -4,16 +4,19 @@ public class Solution {
 
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		int a=0;
-		int b=1;
-		for(int i=1;i<=n;i++) {
-			int c=a+b;
-			a=b;
-			b=c;
-		}
-		System.out.println(a);
-		
+        int t=scan.nextInt();
+        for(int j=0;j<t;j++){
+            int n=scan.nextInt();
+            int a=1;
+            int b=1;
+            int c=1;
+            for(int i=2;i<n;i++) {
+                c=a+b;
+                a=b;
+                b=c;
+            }
+            System.out.println(c);
+        }
 	}
 
 }
